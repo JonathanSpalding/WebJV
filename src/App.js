5ed7517c3132805e15898aa20c7471bcf3d0f28a
JonathanSpalding <Jon.Spalding@hotmail.com>
2018-07-14 13:57:59 -0600

    Add ping function. (Currently broken)

--- src/App.js
+++ src/App.js
@@ -24,7 +24,15 @@ class App extends Component {
   _onRunClicked(event) {
     
     alert("Hello world!");
-
+    var ping = require('ping');
+ 
+    var hosts = ['192.168.1.1', 'google.com', 'yahoo.com'];
+    hosts.forEach(function(host){
+        ping.sys.probe(host, function(isAlive){
+            var msg = isAlive ? 'host ' + host + ' is alive' : 'host ' + host + ' is dead';
+            console.log(msg);
+        });
+    });
   }
   render() {
     return (
